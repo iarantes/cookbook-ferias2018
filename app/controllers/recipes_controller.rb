c83f1308efa2bdbd201d186d0cd91333704fc18b
Lili e Igor <igor_arantes@yahoo.com.br>
2018-07-03 20:55:07 -0300

    Usuário usa cozinhas cadastradas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,10 +6,11 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new()
+    @cuisine = Cuisine.all
   end
 
   def create
-    @recipe = Recipe.create(params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method))
+    @recipe = Recipe.create(params.require(:recipe).permit(:title, :recipe_type, :cuisine_id, :difficulty, :cook_time, :ingredients, :cook_method))
     redirect_to recipe_path(@recipe)
   end  
 end
